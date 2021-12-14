c3263c15f18342906fa8c0ea0762e45282ec43ac
Josh Teitz <josh.teitz@icloud.com>
2021-12-14 15:56:03 -0700

    test_collections

---
+++ load_data.R
@@ -0,0 +1,133 @@
+library(conflicted)
+library(tidyverse)
+library(readxl)
+library(here)
+library(drc)
+library(progress)
+
+conflict_prefer("filter", "dplyr")
+conflict_prefer("select", "dplyr")
+conflict_prefer("map", "purrr")
+
+### Introduction ###################################################################################################
+# This file contains functions for loading data from the "Supplementary Materials" of the paper
+# "Thermal proximity coaggregation for system-wide profiling of protein complex dynamics in cells" by Tan et al..
+# This paper is accessible from the following url: https://science.sciencemag.org/content/359/6380/1170.
+#
+# In order to use this file:
+#   1. Make sure there is a folder called "data" in the same location as "load_data.R"
+#   2. Download "Tables S1 to S27" from https://science.sciencemag.org/content/suppl/2018/02/07/science.aan0346.DC1
+#   2. Unzip folder and place it into "data" folder.
+#   3. Ensure the folder is named "TablesS1_to_S27" and contains two Excel files (.xlsx) named
+#      "Tables_S1_to_S18.xlsx" and "Tables_S19_to_S27.xlsx".
+#   ** If either Excel file is open, the code will not run.
+####################################################################################################################
+
+# Location of the two Excel spreadsheets.
+LOC <- here("data", "TablesS1_to_S27")
+
+# Load melting data from a cell line.
+load_mdata <- function(cell_line = "K562") {
+  file_sheet = switch(
+    cell_line,
+    "K562 lysate" = c("Tables_S1_to_S18.xlsx", "Table S6"),
+    "K562" = c("Tables_S1_to_S18.xlsx", "Table S7"),
+    "A375" = c("Tables_S19_to_S27.xlsx", "Table S19"),
+    "HCT116" = c("Tables_S19_to_S27.xlsx", "Table S20"),
+    "HEK293T" = c("Tables_S19_to_S27.xlsx", "Table S21"),
+    "HL60" = c("Tables_S19_to_S27.xlsx", "Table S22"),
+    "MCF7" = c("Tables_S19_to_S27.xlsx", "Table S23"),
+    "mouse liver" = c("Tables_S19_to_S27.xlsx", "Table S24")
+  )
+  if (is.null(file_sheet)) {
+    stop("Unrecognizable cell line! Please enter one of: \"K562 lysate\", \"K562\", \"A375\", \"HCT116\",
+         \"HEK293T\", \"HL60\", \"MCF7\", or \"mouse liver\".")
+  }
+  mdata <- read_excel(paste0(LOC, "/", file_sheet[1]), sheet = file_sheet[2], skip = 2) %>%
+    select(Protein = Accession, matches("^T\\d+$"))
+  return(mdata)
+}
+
+# Load protein-protein interactions 
+load_interactions <- function() {
+  
+  # file/sheet that contains interactions
+  file_ips <- "Tables_S1_to_S18.xlsx"
+  sheet_ips <- "Table S2"
+  
+  # load data
+  ips <- read_excel(paste0(LOC, "/", file_ips), sheet = sheet_ips, skip = 2) %>%
+    select(ProteinA = `Protein A`, ProteinB = `Protein B`, Num_pubs = `No. of Publications`)
+  
+  # make sure ProteinA < ProteinB and make sure there are no duplicate interactions.
+  ips <- ips %>% mutate(Proteins = map2(ProteinA, ProteinB, ~ sort(c(.x, .y)))) %>%
+    mutate(ProteinA = map_chr(Proteins, ~ .x[1])) %>%
+    mutate(ProteinB = map_chr(Proteins, ~ .x[2])) %>%
+    select(-Proteins) %>%
+    distinct(ProteinA, ProteinB, .keep_all = T)
+  
+  return(ips)
+}
+
+# Load CORUM protein complex data
+load_cdata <- function() {
+  cdata <- read_excel(paste0(LOC, "/Tables_S1_to_S18.xlsx"), sheet = "Table S8", skip = 2) %>%
+    select(Complex = Complex.id, Proteins = subunits..UniProt.IDs.) %>%
+    mutate(Complex = Complex %>% as.integer) %>%
+    mutate(Proteins = strsplit(Proteins, ","))
+  cdata <- map2_dfr(cdata$Complex, cdata$Proteins, ~ tibble(Complex = .x, Protein = .y))
+  return(cdata)
+}
+
+# For each protein's melting data, fit a three parameter log-logistic function with upper limit equal to 1.
+# Then extract the function's three parameters.
+param_mdata <- function(mdata) {
+  pb <- progress_bar$new(total = nrow(mdata))
+  mparams <- convert_long(mdata) %>%
+    group_by(Protein) %>%
+    nest() %>%
+    mutate(Mod = map(data, ~ {pb$tick(); get_drm(.x)})) %>%
+    select(-data) %>%
+    ungroup() %>%
+    mutate(Param_b = map_dbl(Mod, ~ .x$coefficients[1])) %>%
+    mutate(Param_c = map_dbl(Mod, ~ .x$coefficients[2])) %>%
+    mutate(Param_e = map_dbl(Mod, ~ .x$coefficients[3])) %>%
+    select(-Mod)
+  return(mparams)
+}
+
+# Make specified columns unit variance.
+make_unit_var <- function(mdata, numeric_cols = NULL) {
+  scale1 <- function(x) {
+    x_sd <- sd(x)
+    x_mean <- mean(x)
+    return((x - x_mean) / x_sd)
+  }
+  mdata_unit_var <- 
+    mdata %>%
+    mutate(across({{numeric_cols}}, scale1))
+  return(mdata_unit_var)
+}
+
+# Convert melting data to long format
+convert_long <- function(mdata) {
+  mdata_long <- mdata %>%
+    pivot_longer(
+      cols = matches("T\\d+$"), 
+      names_to = "Temp", 
+      values_to = "Sol", 
+      names_prefix = "T", 
+      names_transform = list(Temp = as.integer)
+    )
+  return(mdata_long)
+}
+
+# Wrapper around `drm` function that fails gracefully when a dose response model cannot be made.
+get_drm <- function(df) {
+  tryCatch(
+    drm(Sol ~ Temp, data = df, fct = LL.3u()),
+    error = function(e) {
+      list(coefficients = c(NA, NA, NA))
+    }
+  )
+}
---
+++ test_collections.R
@@ -0,0 +1,190 @@
+library(here)
+
+source(paste0(here(), "/load_data.R"))
+
+# melting data from Table S7
+mdata <- load_mdata()
+
+# convert melting data to parametric melting data
+mparams <- param_mdata(mdata) %>% drop_na()
+
+# pairwise interactions from table S2
+ints <- load_interactions() %>% mutate(Num_pubs = Num_pubs %>% as.integer())
+
+# Remove each interaction where at least one protein does not have parametric melting data
+ints <- ints %>%
+  semi_join(mparams, by = c("ProteinA" = "Protein")) %>%
+  semi_join(mparams, by = c("ProteinB" = "Protein"))
+
+# Ensure there are no duplicate interactions
+ints <- ints %>%
+  mutate(Proteins = map2(ProteinA, ProteinB, ~ sort(c(.x, .y)))) %>%
+  mutate(ProteinA = map_chr(Proteins, ~ .x[1])) %>%
+  mutate(ProteinB = map_chr(Proteins, ~ .x[2])) %>%
+  distinct(ProteinA, ProteinB, .keep_all = T) %>%
+  select(ProteinA, ProteinB, Num_pubs)
+
+# CORUM complex data from table S8
+cdata <- load_cdata()
+
+# Remove each protein with no parametric melting data
+cdata <- cdata %>% semi_join(mparams, by = "Protein")
+
+# pairs of proteins that belong to the same complex
+cints <- cdata %>%
+  nest(Proteins = -Complex) %>%
+  mutate(Proteins = map(Proteins, ~ .x$Protein)) %>%
+  mutate(Interactions = map(Proteins, ~{
+    m <- t( combn(.x, 2) );
+    colnames(m) <- c("ProteinA", "ProteinB");
+    m %>% as_tibble()
+  })) %>%
+  select(Interactions, Complex) %>%
+  unnest(Interactions)
+
+# Ensure there are no duplicate interactions
+cints <- cints %>%
+  mutate(Proteins = map2(ProteinA, ProteinB, ~ sort(c(.x, .y)))) %>%
+  mutate(ProteinA = map_chr(Proteins, ~ .x[1])) %>%
+  mutate(ProteinB = map_chr(Proteins, ~ .x[2])) %>%
+  distinct(ProteinA, ProteinB, .keep_all = T) %>%
+  select(ProteinA, ProteinB, Complex)
+
+# all_ints: union of ints and cints
+all_ints <- bind_rows(ints %>% select(-Num_pubs), cints %>% select(-Complex)) %>%
+  distinct(ProteinA, ProteinB)
+
+# Each protein's interactors
+interactors <- bind_rows(
+  all_ints,
+  all_ints %>% select(ProteinA = ProteinB, ProteinB = ProteinA)
+) %>%
+  nest(Interactors = -ProteinA) %>%
+  mutate(Interactors = map(Interactors, ~ .x$ProteinB %>% unique)) %>%
+  select(Protein = ProteinA, Interactors)
+
+# Data to make test collections.
+# Query: query protein
+# Rel_documents: proteins that interact with with the query as reported by at least two publications.
+# Interactors: proteins that interact with the query according to some evidence.
+test_collections <- bind_rows(
+  ints %>% filter(Num_pubs >= 2) %>% select(ProteinA, ProteinB),
+  ints %>% filter(Num_pubs >= 2) %>% select(ProteinA = ProteinB, ProteinB = ProteinA)
+) %>%
+  nest(Rel_documents = -ProteinA) %>%
+  mutate(Rel_documents = map(Rel_documents, ~ .x$ProteinB %>% unique)) %>%
+  select(Query = ProteinA, Rel_documents) %>%
+  inner_join(interactors, by = c("Query" = "Protein"))
+
+# make_test_collections
+# Arguments:
+#   1. query - the query of the test collection
+#   2. rel_docs - set from which relevant documents are sampled
+#   3. docs <- set form which non-relevant documents are sampled
+#   4. interactors <- all proteins that interact with the query
+#   5. num_rel: number of relevant documents
+#   6. num: number of documents
+make_test_collections <- function(query, rel_docs, docs, interactors, num_rel, num) {
+  
+  # number of test collections that will be made
+  num_tc <- floor(length(rel_docs) / num_rel)
+  
+  if (num_tc < 1) 
+    return(NA)
+  
+  # initialize list of test collections
+  tcs <- vector(mode = "list", length = num_tc)
+  
+  # permute the relevant documents so that we can randomly distribute them in the test collections
+  rel_docs <- sample(rel_docs, size = length(rel_docs)) 
+  
+  # Make the test collections
+  for (i in 1:num_tc) {
+    
+    # range of relevant documents
+    end <- num_rel * i
+    start <- end - num_rel + 1
+    
+    # relevant documents of test collection
+    D_rel <- rel_docs[start:end]
+    
+    # relevant documents that are not chosen for test collection
+    rel_nc <- rel_docs[-(start:end)]
+    
+    # Sample non-relevant documents for test collection
+    D_nrel <- vector(mode = "character", length = num - num_rel)
+    j <- 1
+    while ( j <= (num - num_rel) ) {
+      
+      d <- sample(docs, 1)
+      
+      if ( !(d %in% D_nrel) && !(d %in% interactors) ) {
+        D_nrel[j] <- d
+        j <- j + 1
+      }
+    }
+    
+    # Choose pairs of proteins that are not involved in interactions.
+    prots <- sample(docs) # shuffle proteins
+    pairs <- tibble()
+    for (j in 1:length(prots)) {
+      
+      # choose pair
+      pair <- c(prots[2*j - 1], prots[2*j]) %>% sort
+      
+      # make sure pair is not an interaction
+      if ( (all_ints %>% filter(ProteinA == pair[1] & ProteinB == pair[2]) %>% nrow) == 0 ) {
+        
+        # Add pair to existing pairs
+        pairs <- bind_rows(
+          pairs,
+          tibble(ProteinA = pair[1], ProteinB = pair[2])
+        )
+      }
+      
+      if ( nrow(pairs) == length(rel_nc) )
+        break
+      
+    }
+    
+    # Form test collection.
+    # T_pos/T_neg are pairs of proteins for training weakly supervised metric learners
+    tcs[[i]] <- list(
+      Q = query,
+      D = c(D_rel, D_nrel),
+      R = tibble(Query = query, Document = D_rel),
+      T_pos = tibble(ProteinA = query, ProteinB = rel_nc),
+      T_neg = pairs
+    )
+  }
+  
+  return(tcs)
+}
+
+query <- test_collections$Query[2]
+rel_docs <- test_collections$Rel_documents[[2]]
+docs <- mparams$Protein
+interactors <- test_collections$Interactors[[2]]
+num_rel <- 1
+num <- 20
+make_test_collections(query, rel_docs, docs, interactors, num_rel, num)
+
+# Test collections
+all_docs <- mparams$Protein
+set.seed(27)
+pb <- progress_bar$new(total = nrow(test_collections))
+test_collections <- test_collections %>%
+  mutate(TCs = pmap(list(Query, Rel_documents, Interactors), ~ {
+    pb$tick();
+    make_test_collections(
+      query = ..1,
+      rel_docs = ..2,
+      docs = all_docs,
+      interactors = ..3,
+      num_rel = 1,
+      num = 20
+    )
+  }))
+
+write_rds(test_collections, paste0(here(), "/test_collections.rds"))
+
