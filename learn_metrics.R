0c8bcc144c04b64e58592fc09c1686545c955596
Josh Teitz <josh.teitz@icloud.com>
2021-12-29 15:25:31 -0700

    Make debugging learn_metrics.R easier.

--- learn_metrics.R
+++ learn_metrics.R
@@ -22,28 +22,38 @@ trs_p <- training_sets$Tr_param
 # set.seed(27)
 # inds <- sample.int(nrow(training_sets), 100)
 
+itml_metrics <- tibble(
+  Q = map_chr(training_sets$TC, ~ .x$Q),
+  R_doc = map_chr(training_sets$TC, ~ .x$R$Document),
+  Tr = trs,
+  Tr_p = trs_p
+)
+
+# itml_metrics <- itml_metrics[inds,]
+
 print("Learning ITML metrics...")
 pb <- progress_bar$new(total = nrow(training_sets))
 # pb <- progress_bar$new(total = 100)
-itml_metrics <- tibble(ITML = map(trs, ~ {pb$tick(); learn_itml(.x, min_num = 10, use_neg_pairs = F)}))
+itml_metrics <- itml_metrics %>%
+  mutate(ITML = pmap(list(Q, R_doc, Tr), ~ {pb$tick(); learn_itml(..3, min_num = 10, use_neg_pairs = F)}))
 
 print("Learning ITML metrics with negative training examples...")
 pb <- progress_bar$new(total = nrow(training_sets))
 # pb <- progress_bar$new(total = 100)
 itml_metrics <- itml_metrics %>%
-  mutate(ITML_neg = map(trs, ~ {pb$tick(); learn_itml(.x, min_num = 10, use_neg_pairs = T)}))
+  mutate(ITML_neg = pmap(list(Q, R_doc, Tr), ~ {pb$tick(); print(paste(..1, ..2)); learn_itml(..3, min_num = 10, use_neg_pairs = T)}))
 
-print("Learning ITML metrics on parametric training data")
+print("Learning ITML metrics on parametric training data...")
 pb <- progress_bar$new(total = nrow(training_sets))
 # pb <- progress_bar$new(total = 100)
 itml_metrics <- itml_metrics %>%
-  mutate(ITML_par = map(trs_p, ~ {pb$tick(); learn_itml(.x, min_num = 10, use_neg_pairs = F)}))
+  mutate(ITML_par = pmap(list(Q, R_doc, Tr_p), ~ {pb$tick(); learn_itml(..3, min_num = 10, use_neg_pairs = F)}))
 
 print("Learning ITML metrics on parametric training data with negative training examples")
 pb <- progress_bar$new(total = nrow(training_sets))
 # pb <- progress_bar$new(total = 100)
 itml_metrics <- itml_metrics %>%
-  mutate(ITML_par_neg = map(trs_p, ~ {pb$tick(); learn_itml(.x, min_num = 10, use_neg_pairs = T)}))
+  mutate(ITML_par_neg = pmap(list(Q, R_doc, Tr_p), ~ {pb$tick(); learn_itml(..3, min_num = 10, use_neg_pairs = T)}))
 
 write_rds(itml_metrics, paste0(here(), "/itml_metrics.rds"))
 
