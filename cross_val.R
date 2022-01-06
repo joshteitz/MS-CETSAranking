b536ebac1e931712d44576f00f5d3476b9700870
Josh Teitz <josh.teitz@icloud.com>
2022-01-05 21:23:12 -0700

    Fix to cross_val

--- cross_val.R
+++ cross_val.R
@@ -142,7 +142,7 @@ learn_metrics_cv <- function(cv_ints) {
     itml_neg_metrics[[l]] <- learn_itml(tr, min_num = length(tr$y_pairs), use_neg_pairs = T, verbose = T)
     
     print(paste0("Learning MMC metric for label ", l, "..."))
-    mmc_metrics[[l]] <- learn_mmc(tr, min_num = length(tr$y_pairs), use_neg_pairs = F, diag = F, initialization = "identity", verbose = T)
+    mmc_metrics[[l]] <- learn_mmc(tr, min_num = length(tr$y_pairs), use_neg_pairs = T, diag = F, initialization = "identity", verbose = T)
     
     print(paste0("Learning MMC-neg metric for label ", l, "..."))
     mmc_neg_metrics[[l]] <- learn_mmc(tr, min_num = length(tr$y_pairs), use_neg_pairs = T, diag = T, initialization = "identity", verbose = T)
